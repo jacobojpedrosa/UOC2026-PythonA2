3d7015d183972b120311602c583472e45437bae5
Jacobo J. Pedrosa <jacoboj.pedrosa@Jacobos-Laptop.local>
2026-01-12 12:20:53 +0100

    intro a excepciones

---
+++ MOD2.1-Exceptions/example1.py
@@ -0,0 +1,8 @@
+
+
+try:
+    10/0
+except ZeroDivisionError:
+    print("No se puede dividir entre cero.")
+
+print("hola que tal")
---
+++ MOD2.1-Exceptions/kata-fizzbuz_exceptions/main.py
@@ -0,0 +1,12 @@
+"""
+En esta kata, se solicita al usuario un numero, si este es:
+- Divisible entre 3, el programa debe imprimir "Fizz"
+- Divisible entre 5, el programa debe imprimit "Buzz"
+- Divisible entre 3 y entre 5, el programa debe imprimir "FizzBuzz"
+- Si no es divisible ni entre 3 ni entre 5, debe imprimir el número
+"""
+
+# Iteración 1:
+# 1. Crear un repositorio en GitHub (FizzBuzz Excepciones) -> enviar por chat la url
+# 2. Primera implementación del problema
+# 3. Retro
