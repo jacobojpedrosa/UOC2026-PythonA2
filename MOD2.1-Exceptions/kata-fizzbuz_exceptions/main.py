19f2c89128210024e9004049d9ef7317c7997aa1
Jacobo Pedrosa <jacoboj.pedrosa@gmail.com>
2026-01-19 12:04:06 +0100

    Update comments for project iterations in main.py
    Added comments for project iterations and testing phases.

--- MOD2.1-Exceptions/kata-fizzbuz_exceptions/main.py
+++ MOD2.1-Exceptions/kata-fizzbuz_exceptions/main.py
@@ -10,3 +10,20 @@ En esta kata, se solicita al usuario un numero, si este es:
 # 1. Crear un repositorio en GitHub (FizzBuzz Excepciones) -> enviar por chat la url
 # 2. Primera implementación del problema
 # 3. Retro
+
+
+
+# Iteración 2:
+# 1. Definir y crear al menos 10 tests para ser ejecutados con Pytest. 
+# Estos tests no deben ser funcionales en esta iteración.
+# 3. Retro
+
+
+# Iteración 3:
+# 1. Modificar el código base del proyecto con una function FizzBuzz(n) que reciba un 
+# numero "n" y retorne Fizz o Buzz o FizzBuzz o el numero según la lógica ya definida.
+# 2. Editar uno o varios tests para confirmar que se ejecutan y validan el problema
+# 3. Retro
+
+# Iteración 4:
+# 1. Finalizar la lógica de los tests definidos y validar todo el proyecto
